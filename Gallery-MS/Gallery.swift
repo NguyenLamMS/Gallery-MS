d583a9be612231c2db3960287f7ce1d4049851ad
Nguyen Lam <lam1997.ln@gmail.com>
2019-02-20 14:28:43 +0700

    update

---
+++ Gallery-MS/Gallery.swift
@@ -0,0 +1,127 @@
+//
+//  ViewController.swift
+//  photosApp2
+//
+//  Created by Muskan on 10/4/17.
+//  Copyright © 2017 akhil. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+open class Gallery: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UINavigationControllerDelegate {
+    
+    var myCollectionView: UICollectionView!
+    open var imageArray=[UIImage]()
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Photos"
+        
+        let layout = UICollectionViewFlowLayout()
+        
+        myCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        myCollectionView.delegate=self
+        myCollectionView.dataSource=self
+        myCollectionView.register(PhotoItemCell.self, forCellWithReuseIdentifier: "Cell")
+        myCollectionView.backgroundColor=UIColor.white
+        self.view.addSubview(myCollectionView)
+        
+        myCollectionView.autoresizingMask = UIView.AutoresizingMask(rawValue: UIView.AutoresizingMask.RawValue(UInt8(UIView.AutoresizingMask.flexibleWidth.rawValue) | UInt8(UIView.AutoresizingMask.flexibleHeight.rawValue)))
+        
+    }
+    //MARK: CollectionView
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell=collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! PhotoItemCell
+        cell.img.image=imageArray[indexPath.item]
+        return cell
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath)
+        let vc=ImagePreviewVC()
+        vc.imgArray = self.imageArray
+        vc.passedContentOffset = indexPath
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width
+        if DeviceInfo.Orientation.isPortrait {
+            return CGSize(width: width/4 - 1, height: width/4 - 1)
+        } else {
+            return CGSize(width: width/6 - 1, height: width/6 - 1)
+        }
+    }
+    
+    override open func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        myCollectionView.collectionViewLayout.invalidateLayout()
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+
+    
+    override open func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
+class PhotoItemCell: UICollectionViewCell {
+    
+    var img = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        img.contentMode = .scaleAspectFill
+        img.clipsToBounds=true
+        self.addSubview(img)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        img.frame = self.bounds
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+struct DeviceInfo {
+    struct Orientation {
+        // indicate current device is in the LandScape orientation
+        static var isLandscape: Bool {
+            get {
+                return UIDevice.current.orientation.isValidInterfaceOrientation
+                    ? UIDevice.current.orientation.isLandscape
+                    : UIApplication.shared.statusBarOrientation.isLandscape
+            }
+        }
+        // indicate current device is in the Portrait orientation
+        static var isPortrait: Bool {
+            get {
+                return UIDevice.current.orientation.isValidInterfaceOrientation
+                    ? UIDevice.current.orientation.isPortrait
+                    : UIApplication.shared.statusBarOrientation.isPortrait
+            }
+        }
+    }
+}
---
+++ Gallery-MS/Gallery_MS.h
@@ -0,0 +1,19 @@
+//
+//  Gallery_MS.h
+//  Gallery-MS
+//
+//  Created by Nguyen Lam on 2/20/19.
+//  Copyright © 2019 Binh Phan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gallery_MS.
+FOUNDATION_EXPORT double Gallery_MSVersionNumber;
+
+//! Project version string for Gallery_MS.
+FOUNDATION_EXPORT const unsigned char Gallery_MSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gallery_MS/PublicHeader.h>
+
+
